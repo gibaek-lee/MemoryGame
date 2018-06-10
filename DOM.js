7837dc0198f0232f90bfe3906a2c8fd31caaf637
ddinggu <travelrudwls@gmail.com>
2018-06-11 01:11:03 +0900

    fix bug to quick click

--- DOM.js
+++ DOM.js
@@ -9,14 +9,15 @@ $(document).ready(function(){
                      "level3" : [1,1,2,2,3,3,4,4],
                      "level4" : [1,1,2,2,3,3,4,4,5,5],
                      "level5" : [1,1,2,2,3,3,4,4,5,5,6,6] }); 
-    // dataset을 미리 만들어 배열을 만들 필요 없이 
+    // dataset을 미리 만들어 배열을 만들 필요 없이 JSON형식의 데이터를 불러와서 사용
 
-//   api.pushTotextBox(); // 최초에 textBox에 넣어주는 것을 실행 
-  api.pushBoxWithDataSet();
+//   api.pushTotextBox(); 
+  api.pushBoxWithDataSet(); // 최초에 textBox에 넣어주는 것을 실행 
 
-  $('div .textBox').on('click',api.clickDefalutEvent); // 실행 event(기본 및 두 숫자 확인)
+  $('div .textBox').on('click',_.throttle(api.clickDefalutEvent,800)); // 급하게 빨리 누를 시 버그가 생기는것을 방지 
  
   $('.Resetbutton').on('click', api.reset); // 클릭시 reset 시키는 역활  
 
+  $('.Levelup').on('click', api.templetFactory);
   
 });
