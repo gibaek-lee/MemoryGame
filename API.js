a4cb2bd5a7279575d276f34cee944114c56de0b3
ddinggu <travelrudwls@gmail.com>
2018-06-11 12:12:08 +0900

    debounced click & level configing

--- API.js
+++ API.js
@@ -3,63 +3,93 @@
   api.standard = '';
 
   api.startEvent = function(){
-    $('.textBox').animate({opacity : 0}, 1500); // 시작시 외우라고 보여주기 위해서 만든 함수
-  }
+    $('.textBox').animate({opacity : 0}, 1500);
+  } // 최초 실행시 기본 data를 외우기 위해 보여줘야 함
   
   api.clickDefalutEvent = function(){
-    $(this).css('opacity' ,1); // 기본적으로 클릭하면 none <-> block
+    $(this).css('opacity' ,1); // 기본적으로 클릭하면 내용물을 보여준다. 
     if(api.standard.length === 0 ) {   // 첫번째 클릭인경우(standard가 없는경우) : standard에 값을 지정
       api.standard = $(this).text();    // 두번째 클릭에서의 조건을 돌아가게 해준다.
-      console.log(api.standard);
     }
     else{
       if(api.standard === $(this).text()){ // 두번째 클릭에 맞는경우 display에 대한 변화 X
         api.standard = '';  // 다시 첫번째 클릭 조건으로 돌아가기 위해
-        console.log(api.standard);
-      }
+        }
       else{
         $('.textBox').animate({'opacity': 0},1000); // 두 text값이 다른경우 다시 가려주기 위해 
         api.standard = '';
-        console.log(api.standard);
-      }
+        }
     } 
   };
 
-  api.pushTotextBox = function(){
-    var container = []; 
+  // underscore 라이브러리를 이용해야하기 때문에(_.debounce) 사용 보류 
+  // api.pushTotextBox = function(){
+  //   var container = []; 
     
-    for(var i=1; i<=$('.textBox').length/2; i++){
-      container.push(i);
-      container.push(i);
-    } // 넣어야할 기본 배열을 만들어 준다. 
+  //   for(var i=1; i<=$('.textBox').length/2; i++){
+  //     container.push(i);
+  //     container.push(i);
+  //   } // 넣어야할 기본 배열을 만들어 준다. 
     
-    var shuffledArr = _.shuffle(container); // 배열 섞어주기
+  //   var shuffledArr = _.shuffle(container); // 배열 섞어주기
 
-    for (i=0; i<$('.textBox').length; i++) {
-      var id = '#'+i; // 기존에 id값을 0~n 까지 정한 이유!! 센스 굿!!
-      $(id).append(''+shuffledArr[i]+'').html();
-    } // textBox에 넣어주는 역활을 해준다. (정민님)
-  } 
+  //   for (i=0; i<$('.textBox').length; i++) {
+  //     var id = '#'+i; // 기존에 id값을 0~n 까지 정한 이유!! 센스 굿!!
+  //     $(id).append(''+shuffledArr[i]+'').html();
+  //   } // textBox에 넣어주는 역활을 해준다. (정민님)
+  // } 
+
+  api.vaildationLevel = function(){
+    $('.Levelup')
+  }
+
 
   api.pushBoxWithDataSet = function(){
-    var shuffledArr = _.shuffle($('body').data("levelSetting").level1); // 배열 섞어주기
+    var level1Flag = $('body').data("levelSetting").level1;
+    var level2Flag = $('body').data("levelSetting").level2;
+
+    if(!level1Flag[1]) {
+      level1Flag[1] = true;
+      var dataArr = level1Flag[0];
+    }
+    else if(level1Flag && !level2Flag){
+      level2Flag[1] = true;
+      var dataArr = level2Flag[0];
+    } 
+
+    var shuffledArr = _.shuffle(dataArr); // 배열 섞어주기
 
     for (i=0; i<$('.textBox').length; i++) {
       var id = '#'+i; // 기존에 id값을 0~n 까지 정한 이유!! 센스 굿!!
       $(id).append(''+shuffledArr[i]+'').html();
     }
-
-  }
+  }  
 
   api.reset = function(){
     $('.textBox').empty(); // textBox의 내용물(text값)을 비우는 api
-    // api.pushTotextBox(); // 새롭게 넣어주기 위해 위에 있던 함수를 재실행
     api.pushBoxWithDataSet();
     $('.textBox').css('opacity','1'); // 게임 중간에 reset을 누르는 경우 다시 보여주기 위해서!!
     api.startEvent() // 다시 가리기
+    api.standard = '';
   }
 
-  api.levelup = function(){
+  api.templetFactory = function(){
+    var $childNode1 = $('<div class="childNode"><div id ="4" class="textBox"></div></div>');
+    var $childNode2 = $('<div class="childNode"><div id ="5" class="textBox"></div></div>');
+
+    $('.container').append($childNode1);
+    $('.container').append($childNode2);
+    
+  } // 우선 level2로 넘어가는 경우만 만들기. 
+
+  api.levelUp = function(){
+    var level1Flag = $('body').data("levelSetting").level1;
+    var level2Flag = $('body').data("levelSetting").level2;
+    
+    if(level1Flag[1]){
+      api.templetFactory();
+      api.reset();
+    }
 
   }
       
--- DOM.js
+++ DOM.js
@@ -1,23 +1,25 @@
 $(document).ready(function(){
-
-  // _.delay(api.startEvent, 2000);
-  api.startEvent();
   
   $('body').data("levelSetting",
-                    {"level1" : ['A','A','B','B'],
-                     "level2" : [1,1,2,2,3,3],
-                     "level3" : [1,1,2,2,3,3,4,4],
-                     "level4" : [1,1,2,2,3,3,4,4,5,5],
-                     "level5" : [1,1,2,2,3,3,4,4,5,5,6,6] }); 
+                    {"level1" : [['A','A','B','B'],false],
+                     "level2" : [['A','A','B','B','C','C'],false],
+                     "level3" : [[1,1,2,2,3,3,4,4],false],
+                     "level4" : [[1,1,2,2,3,3,4,4,5,5],false],
+                     "level5" : [[1,1,2,2,3,3,4,4,5,5,6,6],false] 
+                    }); 
     // dataset을 미리 만들어 배열을 만들 필요 없이 JSON형식의 데이터를 불러와서 사용
+    // flag값을 부여해서 level up & down 을 판별할 수 있게 해준다.
+    // http://www.nextree.co.kr/p10155 
+    // data를 미리 설정해주는것과 각 level마다 data를 만드는 것 중 어떤게 효율적일까?? 
+  
+  api.startEvent();  
+  api.pushBoxWithDataSet(); // 최초에 실행되야 하는 함수들 
+  console.log($('body').data("levelSetting").level1[1]);
 
-//   api.pushTotextBox(); 
-  api.pushBoxWithDataSet(); // 최초에 textBox에 넣어주는 것을 실행 
-
-  $('div .textBox').on('click',_.throttle(api.clickDefalutEvent,800)); // 급하게 빨리 누를 시 버그가 생기는것을 방지 
+  $('div .textBox').on('click',_.debounce(api.clickDefalutEvent,200)); // 급하게 빨리 누를 시 버그가 생기는것을 방지 
  
   $('.Resetbutton').on('click', api.reset); // 클릭시 reset 시키는 역활  
 
-  $('.Levelup').on('click', api.templetFactory);
+  $('.Levelup').on('click', api.levelUp);
   
 });
